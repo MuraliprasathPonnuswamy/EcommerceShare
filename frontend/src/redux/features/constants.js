c0d0d32d288c24b0c0930acee4e45a3f1d7c97fb
Muraliprasath-ponnuswamy <mponnus1@asu.edu>
2024-06-26 13:49:25 -0500

    first commit

---
+++ frontend/src/redux/features/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = ""; // We are going to user proxy, that is why it is empty
+export const USERS_URL = "api/users";
+export const CATEGORY_URL = "api/category";
+export const PRODUCT_URL = "api/products";
+export const UPLOAD_URL = "api/upload";
---
+++ frontend/src/redux/features/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { apiSlice } from "../api/apiSlice";
+import authReducer from "./auth/authSlice";
+import favoritesReducer from "../features/favorites/favoriteSlice";
+import { getFavoritesFromLocalStorage } from "../../Utils/localStorage";
+import cartSliceReducer from "../../redux/features/cart/cartSlice";
+
+const initialFavorites = getFavoritesFromLocalStorage() || [];
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    favorites: favoritesReducer,
+    cart: cartSliceReducer,
+  },
+
+  preloadedState: {
+    favorites: initialFavorites,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+setupListeners(store.dispatch);
+export default store;
